b6503e60a9232de4879e58479bf7a55bcf5756dc
kaileyfreedman <kfreedman2023@chadwickschool.org>
2022-02-28 22:49:15 -0800

    worked on tests

--- TestDisplayInfo.cs
+++ TestDisplayInfo.cs
@@ -7,6 +7,18 @@ namespace personalproject2
     {
         public static bool RunTest()
         {
+            Console.WriteLine($"You should see a red F, I, G, H, and T");
+            Program.DisplayInfo("fight", "brown");
+
+            Console.WriteLine($"You should see a green A and a red S, T, I, R");
+            Program.DisplayInfo("stair", "beach");
+
+            Console.WriteLine($"You should see a green K and N and a red I, F, E");
+            Program.DisplayInfo("knife", "known");
+
+            Console.WriteLine($"You should see a green U, G, and H and a red L and A");
+            Program.DisplayInfo("laugh", "cough");
+
             return false;
         }
     }
